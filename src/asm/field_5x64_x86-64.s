265294bf47ebf8998b7e6bafc6cb7280f919c418
Kaushik Nath <csekaushik@yahoo.co.in>
2021-07-20 22:44:10 +0530

    mulx/adcx/adox asm

--- src/asm/field_5x64_x86-64.s
+++ src/asm/field_5x64_x86-64.s
@@ -9,7 +9,7 @@
  * reduced 4-limb forms, then multiply and finally output a half reduced
  * output in 5-limb form. The leading limb is of atmost 33 bits. 
  *
- * Major instructions used in the assemblies: mulx/add/adc.
+ * Major instructions used in the assemblies: mulx/adcx/adox.
  */
 
 .att_syntax
@@ -20,7 +20,7 @@
 .type	secp256k1_fe_mul_inner, %function
 secp256k1_fe_mul_inner:
 movq 	%rsp,%r11
-subq 	$112,%rsp
+subq 	$96,%rsp
 
 movq 	%r11,0(%rsp)
 movq 	%r12,8(%rsp)
@@ -31,122 +31,125 @@ movq 	%rbp,40(%rsp)
 movq 	%rbx,48(%rsp)
 movq 	%rdi,56(%rsp)
 
-movq    0(%rsi),%r8
-movq    8(%rsi),%r9
-movq    16(%rsi),%r10
-movq    24(%rsi),%r11
-movq    0(%rdx),%r12
-movq    8(%rdx),%r13
-movq    16(%rdx),%rdi
-movq    24(%rdx),%r15
-movq    32(%rdx),%rax
+movq    0(%rsi),%rax
+movq    8(%rsi),%rbx
+movq    16(%rsi),%rdi
+
+movq    0(%rdx),%r8
+movq    8(%rdx),%r9
+movq    16(%rdx),%r10
+movq    24(%rdx),%r11
+movq    32(%rdx),%r12
 
 movq    $0x1000003D1,%rdx
 xorq    %rcx,%rcx
-mulx    32(%rsi),%rbx,%rbp
-addq    %rbx,%r8
-adcq    %rbp,%r9
+mulx    32(%rsi),%rbp,%rcx
+movq    24(%rsi),%rsi
+addq    %rbp,%rax
+adcq    %rcx,%rbx
+adcq    $0,%rdi
+adcq    $0,%rsi
+cmovc   %rdx,%rcx
+addq    %rcx,%rax
+adcq    $0,%rbx
+
+xorq    %rcx,%rcx
+mulx    %r12,%r13,%r14
+addq    %r13,%r8
+adcq    %r14,%r9
 adcq    $0,%r10
 adcq    $0,%r11
 cmovc   %rdx,%rcx
 addq    %rcx,%r8
 adcq    $0,%r9
 
-xorq    %rcx,%rcx
-mulx    %rax,%rax,%rbx
-addq    %rax,%r12
-adcq    %rbx,%r13
-adcq    $0,%rdi
-adcq    $0,%r15
-cmovc   %rdx,%rcx
-addq    %rcx,%r12
-adcq    $0,%r13
-movq    %r15,%rsi
-
 movq    %r8,64(%rsp)
 movq    %r9,72(%rsp)
 movq    %r10,80(%rsp)
 movq    %r11,88(%rsp)
-movq    %r12,96(%rsp)
-movq    %r13,104(%rsp)
 
-movq    64(%rsp),%rdx
-mulx    96(%rsp),%r8,%r9
-mulx    104(%rsp),%rcx,%r10
-addq    %rcx,%r9
+xorq    %r13,%r13
+movq    64(%rsp),%rdx    
+mulx    %rax,%r8,%r9
+mulx    %rbx,%rcx,%r10
+adcx    %rcx,%r9     
 mulx    %rdi,%rcx,%r11
-adcq    %rcx,%r10
+adcx    %rcx,%r10    
 mulx    %rsi,%rcx,%r12
-adcq    %rcx,%r11
-adcq    $0,%r12
+adcx    %rcx,%r11
+adcx    %r13,%r12
 
-movq    72(%rsp),%rdx    
-mulx    96(%rsp),%rax,%rbx
-mulx    104(%rsp),%rcx,%rbp
-addq    %rcx,%rbx
-mulx    %rdi,%rcx,%r15
-adcq    %rcx,%rbp
-mulx    %rsi,%rcx,%r13
-adcq    %rcx,%r15
-adcq    $0,%r13
-addq    %rax,%r9
-adcq    %rbx,%r10
-adcq    %rbp,%r11
-adcq    %r15,%r12
-adcq    $0,%r13
+xorq    %r14,%r14
+movq    72(%rsp),%rdx
+mulx    %rax,%rcx,%rbp
+adcx    %rcx,%r9
+adox    %rbp,%r10
+mulx    %rbx,%rcx,%rbp
+adcx    %rcx,%r10
+adox    %rbp,%r11
+mulx    %rdi,%rcx,%rbp
+adcx    %rcx,%r11
+adox    %rbp,%r12
+mulx    %rsi,%rcx,%rbp
+adcx    %rcx,%r12
+adox    %rbp,%r13	
+adcx    %r14,%r13
 
+xorq    %r15,%r15
 movq    80(%rsp),%rdx
-mulx    96(%rsp),%rax,%rbx
-mulx    104(%rsp),%rcx,%rbp
-addq    %rcx,%rbx
-mulx    %rdi,%rcx,%r15
-adcq    %rcx,%rbp
-mulx    %rsi,%rcx,%r14
-adcq    %rcx,%r15
-adcq    $0,%r14
-addq    %rax,%r10
-adcq    %rbx,%r11
-adcq    %rbp,%r12
-adcq    %r15,%r13
-adcq    $0,%r14
+mulx    %rax,%rcx,%rbp
+adcx    %rcx,%r10
+adox    %rbp,%r11
+mulx    %rbx,%rcx,%rbp
+adcx    %rcx,%r11
+adox    %rbp,%r12
+mulx    %rdi,%rcx,%rbp
+adcx    %rcx,%r12
+adox    %rbp,%r13
+mulx    %rsi,%rcx,%rbp
+adcx    %rcx,%r13
+adox    %rbp,%r14
+adcx    %r15,%r14
 
+xorq    %rdx,%rdx
 movq    88(%rsp),%rdx
-mulx    96(%rsp),%rax,%rbx
-mulx    104(%rsp),%rcx,%rbp
-addq    %rcx,%rbx
-mulx    %rdi,%rcx,%r15
-adcq    %rcx,%rbp
-mulx    %rsi,%rcx,%rsi
-adcq    %rcx,%r15
-adcq    $0,%rsi
-addq    %rax,%r11
-adcq    %rbx,%r12
-adcq    %rbp,%r13
-adcq    %r15,%r14
-adcq    $0,%rsi
-
+mulx    %rax,%rcx,%rbp
+adcx    %rcx,%r11
+adox    %rbp,%r12
+mulx    %rbx,%rcx,%rbp
+adcx    %rcx,%r12
+adox    %rbp,%r13
+mulx    %rdi,%rcx,%rbp
+adcx    %rcx,%r13
+adox    %rbp,%r14
+mulx    %rsi,%rcx,%rbp
+adcx    %rcx,%r14
+adox    %rbp,%r15			
+adcq    $0,%r15
+  
+xorq    %rbp,%rbp
 movq    $0x1000003D1,%rdx
-mulx    %r12,%r12,%rbx
-mulx    %r13,%r13,%rcx
-addq    %rbx,%r13
-mulx    %r14,%r14,%rbx
-adcq    %rcx,%r14
-mulx    %rsi,%r15,%rcx
-adcq    %rbx,%r15
-adcq    $0,%rcx
-addq    %r12,%r8
-adcq    %r13,%r9
-adcq    %r14,%r10
-adcq    %r15,%r11
-adcq    $0,%rcx
+mulx    %r12,%rax,%r12 
+adcx    %rax,%r8
+adox    %r12,%r9
+mulx    %r13,%rcx,%r13
+adcx    %rcx,%r9
+adox    %r13,%r10
+mulx    %r14,%rcx,%r14
+adcx    %rcx,%r10
+adox    %r14,%r11
+mulx    %r15,%rcx,%r15
+adcx    %rcx,%r11
+adox    %rbp,%r15
+adcx    %rbp,%r15		
 
-movq 	56(%rsp),%rdi
+movq    56(%rsp),%rdi
 
-movq   	%r8,0(%rdi)
-movq   	%r9,8(%rdi)
-movq   	%r10,16(%rdi)
-movq   	%r11,24(%rdi)
-movq   	%rcx,32(%rdi)
+movq    %r8,0(%rdi)
+movq    %r9,8(%rdi)
+movq    %r10,16(%rdi)
+movq    %r11,24(%rdi)
+movq    %r15,32(%rdi)
 
 movq 	 0(%rsp),%r11
 movq 	 8(%rsp),%r12
@@ -166,7 +169,7 @@ ret
 .type secp256k1_fe_sqr_inner, %function
 secp256k1_fe_sqr_inner:
 movq    %rsp,%r11
-subq    $64,%rsp
+subq    $56,%rsp
 
 movq 	%r11,0(%rsp)
 movq 	%r12,8(%rsp)
@@ -175,100 +178,98 @@ movq 	%r14,24(%rsp)
 movq 	%r15,32(%rsp)
 movq 	%rbp,40(%rsp)
 movq 	%rbx,48(%rsp)
-movq 	%rdi,56(%rsp)
 
-movq    0(%rsi),%rbp
-movq    8(%rsi),%rdi
-movq    16(%rsi),%rcx
+movq    0(%rsi),%rbx  
+movq    8(%rsi),%rbp  
+movq    16(%rsi),%rax
 
 movq    $0x1000003D1,%rdx
 xorq    %r15,%r15
 mulx    32(%rsi),%r13,%r14
 movq    24(%rsi),%rsi
-addq    %r13,%rbp
-adcq    %r14,%rdi
-adcq    $0,%rcx
+addq    %r13,%rbx
+adcq    %r14,%rbp
+adcq    $0,%rax
 adcq    $0,%rsi
 cmovc   %rdx,%r15
-addq    %r15,%rbp
-adcq    $0,%rdi
+addq    %r15,%rbx
+adcq    $0,%rbp
 
-movq    %rbp,%rdx    
-mulx    %rdi,%r9,%r10
-mulx    %rcx,%r8,%r11
-addq    %r8,%r10
-mulx    %rsi,%rdx,%r12
-adcq    %rdx,%r11
-adcq    $0,%r12
+xorq    %r13,%r13
+movq    %rbx,%rdx
+mulx    %rbp,%r9,%r10
+mulx    %rax,%rcx,%r11
+adcx    %rcx,%r10
+mulx    %rsi,%rcx,%r12
+adcx    %rcx,%r11
+adcx    %r13,%r12
 
-movq    %rdi,%rdx
-mulx    %rcx,%rax,%rbx
-mulx    %rsi,%rdx,%r13
-addq    %rdx,%rbx
-adcq    $0,%r13
-addq    %rax,%r11
-adcq    %rbx,%r12
-adcq    $0,%r13
+xorq    %r14,%r14
+movq    %rbp,%rdx
+mulx    %rax,%rcx,%rdx
+adcx    %rcx,%r11
+adox    %rdx,%r12
+movq    %rbp,%rdx
+mulx    %rsi,%rcx,%rdx
+adcx    %rcx,%r12
+adox    %rdx,%r13
+adcx    %r14,%r13
 
-movq    %rcx,%rdx
-mulx    %rsi,%rax,%r14
-addq    %rax,%r13
-adcq    $0,%r14
+xorq    %r15,%r15
+movq    %rax,%rdx
+mulx    %rsi,%rcx,%r14
+adcx    %rcx,%r13
+adcx    %r15,%r14
 
-movq    $0,%r15
 shld    $1,%r14,%r15
 shld    $1,%r13,%r14
 shld    $1,%r12,%r13
 shld    $1,%r11,%r12
 shld    $1,%r10,%r11
 shld    $1,%r9,%r10
-shlq    $1,%r9
+addq    %r9,%r9
+     
+xorq    %rdx,%rdx
+movq    %rbx,%rdx
+mulx    %rdx,%r8,%rdx
+adcx    %rdx,%r9
 
 movq    %rbp,%rdx
-mulx    %rdx,%r8,%rax
-addq    %rax,%r9
+mulx    %rdx,%rcx,%rdx
+adcx    %rcx,%r10
+adcx    %rdx,%r11
 
-movq    %rdi,%rdx
-mulx    %rdx,%rax,%rbx
-adcq    %rax,%r10
-adcq    %rbx,%r11
-
-movq    %rcx,%rdx
-mulx    %rdx,%rax,%rbx
-adcq    %rax,%r12
-adcq    %rbx,%r13
+movq    %rax,%rdx
+mulx    %rdx,%rcx,%rdx
+adcx    %rcx,%r12
+adcx    %rdx,%r13
 
 movq    %rsi,%rdx
-mulx    %rdx,%rax,%rbx
-adcq    %rax,%r14
-adcq    %rbx,%r15
+mulx    %rdx,%rcx,%rdx
+adcx    %rcx,%r14
+adcx    %rdx,%r15	
 
+xorq    %rbx,%rbx
 movq    $0x1000003D1,%rdx
+mulx    %r12,%r12,%rbp
+adcx    %r8,%r12
+adox    %r9,%rbp
+mulx    %r13,%rcx,%rax
+adcx    %rcx,%rbp
+adox    %r10,%rax
+mulx    %r14,%rcx,%rsi
+adcx    %rcx,%rax
+adox    %r11,%rsi
+mulx    %r15,%rcx,%r15
+adcx    %rcx,%rsi
+adox    %rbx,%r15
+adcx    %rbx,%r15	
 
-mulx    %r12,%r12,%rbx
-mulx    %r13,%r13,%rcx
-addq    %rbx,%r13
-
-mulx    %r14,%r14,%rbx
-adcq    %rcx,%r14
-
-mulx    %r15,%r15,%rcx
-adcq    %rbx,%r15
-adcq    $0,%rcx
-
-addq    %r12,%r8
-adcq    %r13,%r9
-adcq    %r14,%r10
-adcq    %r15,%r11
-adcq    $0,%rcx
-
-movq 	56(%rsp),%rdi
-
-movq   	%r8,0(%rdi)
-movq   	%r9,8(%rdi)
-movq   	%r10,16(%rdi)
-movq   	%r11,24(%rdi)
-movq   	%rcx,32(%rdi)
+movq    %r12,0(%rdi)
+movq    %rbp,8(%rdi)
+movq    %rax,16(%rdi)
+movq    %rsi,24(%rdi)
+movq    %r15,32(%rdi)
 
 movq 	 0(%rsp),%r11
 movq 	 8(%rsp),%r12
